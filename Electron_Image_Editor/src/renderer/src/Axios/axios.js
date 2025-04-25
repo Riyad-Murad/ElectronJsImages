12f56073ecab01b5ecd9a5398deb44efcc0ec830
Riyad Murad <riyadmurad01@gmail.com>
2025-04-25 18:45:57 +0300

    pull request

--- Electron_Image_Editor/src/renderer/src/Axios/axios.js
+++ Electron_Image_Editor/src/renderer/src/Axios/axios.js
@@ -3,7 +3,9 @@ import axios from 'axios'
 const token = localStorage.getItem("Token");
 
 const axiosBaseUrl = axios.create({
-  baseURL: "http://15.237.215.230:8000/api/v1",
+  baseURL: "http://35.180.33.185:8000/api/v1",
+  // baseURL: "http://15.237.119.184:8000/api/v1",
+  // baseURL: "http://15.237.215.230:8000/api/v1",
   // baseURL: "http://localhost:8000/api/v1",
 
   headers: {
